c908f51252e56946e5cb7e9291c2bc324dea87ac
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2023-05-17 21:55:32 +0200

    Add files via upload

--- Searcher/opener.js
+++ Searcher/opener.js
@@ -1,4 +1,15 @@
 
-function openWindow(){
-    
+
+const WindowList = []
+const UrlList = []
+
+const bing_str = 'https://www.bing.com/search?q='
+const google_str = 'https://www.google.de/search?q='
+const duck_str = 'https://duckduckgo.com/?q='
+
+
+function openWindow(url, what){
+    target = encodeURI(url + what)
+    WindowList.push(window.open(target))
+    UrlList.push(decodeURI(target))
 }
