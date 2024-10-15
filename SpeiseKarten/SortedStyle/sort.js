4539234a145b2cde4b4142f03656514e076a2a8d
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2024-10-15 08:04:08 +0200

    Add files via upload

--- SpeiseKarten/SortedStyle/sort.js
+++ SpeiseKarten/SortedStyle/sort.js
@@ -1,40 +1,65 @@
 let memory = []
-function save ( div = document.createElement('div') ) {
-    let main    = []
+function save(div = document.createElement('div')) {
+    let main = []
     for (let index = 3; index < div.children.length; index += 3) {
-        let sub     = []
-        sub.push( div.children[index].cloneNode(true), div.children[index + 1].cloneNode(true) , div.children[index + 2].cloneNode(true)  )
-        main.push( sub )
+        let sub = []
+        sub.push(div.children[index].cloneNode(true), div.children[index + 1].cloneNode(true), div.children[index + 2].cloneNode(true))
+        main.push(sub)
     }
     memory = main
 }
-function remove ( div = document.createElement('div') ) {
-    for(let i = div.children.length -1 ; i > 2 ; i-- ) {
+function remove(div = document.createElement('div')) {
+    for (let i = div.children.length - 1; i > 2; i--) {
         div.children[i].remove()
     }
 }
-function sort( mode = 0 ) {
+function sort(mode = 0) {
+
     switch (mode) {
         case 0:
-            memory.sort( ( a, b ) => a[0].innerText.localeCompare(b[0].innerText) )        
+            memory.sort((a, b) => a[0].innerText.localeCompare(b[0].innerText))
             break;
         case 1:
-            memory.sort( ( a, b ) => a[1].innerText.localeCompare(b[1].innerText) )
+            memory.sort((a, b) => a[1].innerText.localeCompare(b[1].innerText))
             break;
         case 2:
-            memory.sort( ( a, b ) => Number.parseFloat(a[2].innerText) - Number.parseFloat(b[2].innerText) )
+            memory.sort((a, b) => Number.parseFloat(a[2].innerText) - Number.parseFloat(b[2].innerText))
             break;
         default:
             break;
     }
 }
-function insert ( div ) {
-    div.append( ...memory.flat() )
+
+function sort2(mode = 0) {
+    switch (mode) {
+        case 0:
+            memory.sort((a, b) => b[0].innerText.localeCompare(a[0].innerText))
+            break;
+        case 1:
+            memory.sort((a, b) => b[1].innerText.localeCompare(a[1].innerText))
+            break;
+        case 2:
+            memory.sort((a, b) => Number.parseFloat(b[2].innerText) - Number.parseFloat(a[2].innerText))
+            break;
+        default:
+            break;
+    }
+}
+function insert(div) {
+    div.append(...memory.flat())
 }
 
-function action (div, mode) {
-      save(div)
+function action(div, mode) {
+    save(div)
     remove(div)
-      sort(mode)
+    if (div.mode == undefined) {
+        sort(mode)
+        div.mode = mode        
+    } else {
+        sort2(mode)
+        div.mode = undefined
+    }
+    
+
     insert(div)
 }
